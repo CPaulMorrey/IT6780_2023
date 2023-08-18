67e77c0c4c7ae25093d67a1ab92c7b1457b6a624
CPaulMorrey <56007942+CPaulMorrey@users.noreply.github.com>
2023-08-17 18:00:42 -0600

    Clarified comments in class

--- gitDemo.bat
+++ gitDemo.bat
@@ -1,5 +1,8 @@
 REM	gitDemo.bat
 
+REM 1) Add your name (or nickname) to the list.
+REM 2) Check the file in on GitHub
+
 REM *****************************************
 ECHO off
 CLS
@@ -7,7 +10,3 @@ ECHO gitDemo.bat - Demonstration
 ECHO Successfully demonstrated by the following:
 ECHO C. Paul Morrey
 REM *****************************************
-
-REM 1) Add your name (or nickname) to the list.
-REM 2) Check the file in on GitHub
-
